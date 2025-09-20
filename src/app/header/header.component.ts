440ee3f9169b288b13bbceb6aeef3c028bbaf8c5
Vasanth K <vivivijay57@gmail.com>
2025-09-20 12:35:11 +0530

    feat : header changes done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  standalone:false
+  standalone: false
 })
 export class HeaderComponent implements OnInit {
+  hideHeader = false;
+  private lastScrollTop = 0;  
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (st > this.lastScrollTop) {
+      // scrolling down → hide
+      this.hideHeader = true;
+    } else {
+      // scrolling up → show
+      this.hideHeader = false;
+    }
+    this.lastScrollTop = st <= 0 ? 0 : st; // avoid negative
+  }
+
+  
   constructor() { }
 
   ngOnInit() {
